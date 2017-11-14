a0b179c119d0697a799165335755cde9a3597c5c
Cyrill Gorcunov <gorcunov@gmail.com>
2017-11-14 22:51:26 +0300

    gate: k8s -- Diable resources for a while
    | spec.template.spec.containers[0].resources.limits[limits.cpu]:
    | Invalid value: "limits.cpu": must be a standard resource for
    | containers, spec.template.spec.containers[0].resources.limits[limits.memory]:
    | Invalid value: "limits.memory": must be a standard resource for containers,
    | ...
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- src/gate/k8s.go
+++ src/gate/k8s.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"k8s.io/client-go/pkg/api/resource"
+//	"k8s.io/client-go/pkg/api/resource"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/pkg/api/unversioned"
 	"k8s.io/client-go/pkg/api/v1"
@@ -311,17 +311,17 @@ func swk8sRun(conf *YAMLConf, fn *FunctionDesc, fi *FnInst) error {
 				})
 	}
 
-	resources := RtGetFnResources(fn)
+//	resources := RtGetFnResources(fn)
 
-	/* FIXME -- check for parse errors */
-	resspec := v1.ResourceRequirements {
-		Limits: v1.ResourceList{
-			v1.ResourceLimitsMemory:	resource.MustParse(resources["mem.lim"]),
-			v1.ResourceRequestsMemory:	resource.MustParse(resources["mem.req"]),
-			v1.ResourceLimitsCPU:		resource.MustParse(resources["cpu.lim"]),
-			v1.ResourceRequestsCPU:		resource.MustParse(resources["cpu.req"]),
-		},
-	}
+//	/* FIXME -- check for parse errors */
+//	resspec := v1.ResourceRequirements {
+//		Limits: v1.ResourceList{
+//			v1.ResourceLimitsMemory:	resource.MustParse(resources["mem.lim"]),
+//			v1.ResourceRequestsMemory:	resource.MustParse(resources["mem.req"]),
+//			v1.ResourceLimitsCPU:		resource.MustParse(resources["cpu.lim"]),
+//			v1.ResourceRequestsCPU:		resource.MustParse(resources["cpu.req"]),
+//		},
+//	}
 
 	podspec := v1.PodTemplateSpec{
 		ObjectMeta:	v1.ObjectMeta {
@@ -355,7 +355,7 @@ func swk8sRun(conf *YAMLConf, fn *FunctionDesc, fi *FnInst) error {
 						},
 					},
 					ImagePullPolicy: v1.PullNever,
-					Resources:	resspec,
+					//Resources:	resspec,
 				},
 			},
 		},
