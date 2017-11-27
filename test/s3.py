5eb29066c6baabe27242ee45f6e6227152464719
Cyrill Gorcunov <gorcunov@gmail.com>
2017-11-28 01:32:19 +0300

    s3: Shrink test
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- test/s3.py
+++ test/s3.py
@@ -16,33 +16,28 @@ access_key = '6DLA43X797XL2I42IJ33'
 secret_key = 'AJwz9vZpdnz6T5TqEDQOEFos6wxxCnW0qwLQeDcB'
 endpoint_url = 'http://localhost:8787/'
 
-bucket_name = 'bucket-1'
+print("Connecting to endpoint %s with keys %s / %s" %
+      (endpoint_url, access_key, secret_key))
+
 s3 = boto3.session.Session().client(service_name = 's3',
                                     aws_access_key_id = access_key,
                                     aws_secret_access_key = secret_key,
                                     endpoint_url = endpoint_url)
 
+bucket_name = genBucketName()
 s3.create_bucket(Bucket = bucket_name)
-#s3.delete_bucket(Bucket = bucket_name)
-#response = s3.list_buckets(Bucket = bucket_name)
-#print(response)
 
-with open(os.getcwd() + '/test/s3.py', 'rb') as data:
-    s3.put_object(Bucket = bucket_name, Key = 's3-1.py', Body = data)
-    response = s3.get_object(Bucket = bucket_name, Key = 's3-1.py')
-    print(response)
-    if response['ContentLength'] > 0:
-        body = response['Body']
-        print(body.read())
-    respose = s3.delete_object(Bucket = bucket_name, Key = 's3-1.py')
-    s3.delete_bucket(Bucket = bucket_name)
-    #print(response)
+object_name = genObjectName()
+object_body = genRandomData(32)
+s3.put_object(Bucket = bucket_name, Key = object_name, Body = object_body)
 
-#with open('test/s3.py', 'rb') as data:
-#    s3.put_object(Bucket = bucket_name, Key = 's3-2.py', Body = data)
+response = s3.get_object(Bucket = bucket_name, Key = object_name)
+if response['ContentLength'] > 0:
+    body = response['Body'].read().decode("utf-8")
+    if body == object_body:
+        print("PASS: %s" % (object_body))
+    else:
+        print("FAIL: expected %s but got %s" % (object_body, body))
 
-#s3.delete_object(Bucket = bucket_name, Key = key1)
-#s3.delete_object(Bucket = bucket_name, Key = key2)
-
-#response = s3.list_objects(Bucket = bucket_name)
-#print(response)
+#s3.delete_object(Bucket = bucket_name, Key = object_name)
+#s3.delete_object(Bucket = bucket_name, Key = object_name)
