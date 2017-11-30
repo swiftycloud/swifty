547cb9b6e8ba505501d9cd128b8bbe2ed7d4fcde
Cyrill Gorcunov <gorcunov@gmail.com>
2017-12-01 01:41:32 +0300

    s3: Drop no loner needed member helper
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- src/s3/helpers.go
+++ src/s3/helpers.go
@@ -6,22 +6,6 @@ import (
 	"strings"
 )
 
-func member(source, start_with, end_with string) string {
-	var start, stop int
-
-	start = strings.Index(source, start_with)
-	if start >= 0 {
-		start += len(start_with)
-		stop = strings.Index(source[start:], end_with)
-		if stop > 0 {
-			stop += start
-			return source[start:stop]
-		}
-	}
-
-	return ""
-}
-
 func HTTPMarshalXMLAndWrite(w http.ResponseWriter, status int, data interface{}) error {
 	xdata, err := xml.Marshal(data)
 	if err != nil {
