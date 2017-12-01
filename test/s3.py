05b5e038267232de2fe6535abd60f25ccb66fc7a
Cyrill Gorcunov <gorcunov@gmail.com>
2017-12-01 13:20:55 +0300

    s3: test -- Add arguments
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- test/s3.py
+++ test/s3.py
@@ -1,3 +1,4 @@
+import argparse
 import boto3
 import random
 import string
@@ -12,33 +13,47 @@ def genBucketName():
 def genObjectName():
     return genRandomData(10)
 
-access_key = '6DLA43X797XL2I42IJ33'
-secret_key = 'AJwz9vZpdnz6T5TqEDQOEFos6wxxCnW0qwLQeDcB'
-#endpoint_url = 'http://localhost:8787/'
-endpoint_url = 'http://192.168.122.197:8787/'
+parser = argparse.ArgumentParser()
+parser.add_argument('--access-key', dest = 'access_key',
+                    default = '6DLA43X797XL2I42IJ33',
+                    help = 'access key')
+parser.add_argument('--secret-key', dest = 'secret_key',
+                    default = 'AJwz9vZpdnz6T5TqEDQOEFos6wxxCnW0qwLQeDcB',
+                    help = 'secret key')
+parser.add_argument('--endpoint-url', dest = 'endpoint_url',
+                    default = 'http://192.168.122.197:8787/',
+                    help = 'S3 service address')
+parser.add_argument('--bucket-name', dest = 'bucket_name',
+                    default = genBucketName(),
+                    help = 'bucket name to use')
+parser.add_argument('--object-name', dest = 'object_name',
+                    default = genObjectName(),
+                    help = 'object name to use')
+parser.add_argument('--object-body', dest = 'object_body',
+                    default = genRandomData(32),
+                    help = 'object body to use')
+args = parser.parse_args()
 
 print("Connecting to endpoint %s with keys %s / %s" %
-      (endpoint_url, access_key, secret_key))
+      (args.endpoint_url, args.access_key, args.secret_key))
 
 s3 = boto3.session.Session().client(service_name = 's3',
-                                    aws_access_key_id = access_key,
-                                    aws_secret_access_key = secret_key,
-                                    endpoint_url = endpoint_url)
+                                    aws_access_key_id = args.access_key,
+                                    aws_secret_access_key = args.secret_key,
+                                    endpoint_url = args.endpoint_url)
 
-bucket_name = genBucketName()
-s3.create_bucket(Bucket = bucket_name)
+print("Creating bucket %s with object %s" % (args.bucket_name, args.object_name))
+s3.create_bucket(Bucket = args.bucket_name)
 
-object_name = genObjectName()
-object_body = genRandomData(32)
-s3.put_object(Bucket = bucket_name, Key = object_name, Body = object_body)
+s3.put_object(Bucket = args.bucket_name, Key = args.object_name, Body = args.object_body)
 
-response = s3.get_object(Bucket = bucket_name, Key = object_name)
+response = s3.get_object(Bucket = args.bucket_name, Key = args.object_name)
 if response['ContentLength'] > 0:
     body = response['Body'].read().decode("utf-8")
-    if body == object_body:
-        print("PASS: %s" % (object_body))
+    if body == args.object_body:
+        print("PASS: %s" % (args.object_body))
     else:
-        print("FAIL: expected %s but got %s" % (object_body, body))
+        print("FAIL: expected %s but got %s" % (args.object_body, body))
 
-#s3.delete_object(Bucket = bucket_name, Key = object_name)
-#s3.delete_bucket(Bucket = bucket_name)
+#s3.delete_object(Bucket = args.bucket_name, Key = args.object_name)
+#s3.delete_bucket(Bucket = args.bucket_name)
