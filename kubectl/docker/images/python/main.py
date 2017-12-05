dfc416259fcbb97ab8af3e7af6611ce6a29a8d50
Pavel Emelyanov <ovzxemul@gmail.com>
2017-12-05 11:30:00 +0300

    py: Remove old stuff
    Signed-off-by: Pavel Emelyanov <ovzxemul@gmail.com>

--- kubectl/docker/images/python/main.py
+++
@@ -1,9 +0,0 @@
-# Gate puts downloaded sources into /function/code/
-# and for single file it's name is script.py
-import sys
-import json
-from code import script
-
-ret = script.main(json.loads(sys.argv[1]))
-with open("/dev/shm/swyresult.json", 'w') as outf:
-    json.dump(ret, outf)
