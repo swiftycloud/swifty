e7a8a0262c9866d48490fa11853cd370703db88c
Pavel Emelyanov <ovzxemul@gmail.com>
2018-01-19 16:59:28 +0000

    test: Use swifty lib for mongo
    Signed-off-by: Pavel Emelyanov <ovzxemul@gmail.com>

--- test/functions/python/mongo.py
+++ test/functions/python/mongo.py
@@ -1,15 +1,9 @@
 from pymongo import MongoClient
 import os
+import swifty
 
 def main(args):
-    mwn = args['dbname'].upper()
-    dbaddr = os.getenv('MWARE_' + mwn + '_ADDR')
-    dbuser = os.getenv('MWARE_' + mwn + '_USER')
-    dbpass = os.getenv('MWARE_' + mwn + '_PASS')
-    dbname = os.getenv('MWARE_' + mwn + '_DBNAME')
-    connstr = 'mongodb://%s:%s@%s/%s' % (dbuser, dbpass, dbaddr, dbname)
-    client = MongoClient(connstr)
-    db = client[dbname]
+    db = swifty.MongoDatabase(args['dbname'])
     colname = args['collection']
     col = db[colname]
 
