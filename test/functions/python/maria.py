ecfa7963717e55186576425522a7d804a287dbb4
Pavel Emelyanov <ovzxemul@gmail.com>
2018-01-19 15:48:53 +0000

    test: Use swifty lib for maria
    Throw away tons of code
    
    Signed-off-by: Pavel Emelyanov <ovzxemul@gmail.com>

--- test/functions/python/maria.py
+++ test/functions/python/maria.py
@@ -1,29 +1,9 @@
 import pymysql.cursors
 import os
-
-dbconn = None
-
-def gcon(args):
-    global dbconn
-    if dbconn == None:
-        print("New connection")
-        mwn = args['dbname'].upper()
-        x = os.getenv('MWARE_' + mwn + '_ADDR').split(":")
-        dbaddr = x[0]
-        dbport = int(x[1])
-        dbuser = os.getenv('MWARE_' + mwn + '_USER')
-        dbpass = os.getenv('MWARE_' + mwn + '_PASS')
-        dbname = os.getenv('MWARE_' + mwn + '_DBNAME')
-        print("Connect to %s:%s@%s:%d/%s" % (dbuser, dbpass, dbaddr, dbport, dbname))
-        dbconn = pymysql.connect(host=dbaddr, port=dbport,
-                        user=dbuser, password=dbpass, db=dbname,
-                        cursorclass=pymysql.cursors.DictCursor)
-    else:
-        print("Reuse connection")
-    return dbconn
+import swifty
 
 def main(args):
-    db = gcon(args)
+    db = swifty.MariaConn(args['dbname'])
     res = "invalid"
     with db.cursor() as cursor:
         if args['action'] == 'create':
