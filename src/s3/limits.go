1aacf7baec15837d5dd156f193ebe1333992d7b8
Cyrill Gorcunov <gorcunov@gmail.com>
2018-02-01 13:44:36 +0300

    s3: limits -- Add comments and S3BucketMaxACL
    Will need to handle acls
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- src/s3/limits.go
+++ src/s3/limits.go
@@ -1,7 +1,16 @@
 package main
 
 const (
+	// Maximum of objects in a bucket
 	S3StorageMaxObjects		= int64(10000)
+
+	// Maximum size of one object
 	S3StorageMaxBytes		= int64(100 << 20)
+
+	// Maximum size of an object to keep data inside
+	// MongoDB itself
 	S3StorageSizePerObj		= int64(16 << 20)
+
+	// Maximum ACL per bucket/object
+	S3BucketMaxACL			= int(100)
 )
