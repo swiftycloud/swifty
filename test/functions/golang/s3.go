13039c8daf61cc0b460d0a269cb3c1529efdcb6f
Pavel Emelyanov <ovzxemul@gmail.com>
2018-06-28 15:09:22 +0000

    test: Extend s3 function to list, get and put objects
    Signed-off-by: Pavel Emelyanov <ovzxemul@gmail.com>

--- test/functions/golang/s3.go
+++ test/functions/golang/s3.go
@@ -1,68 +1,88 @@
 package main
 
 import (
-	"os"
 	"fmt"
+	"strings"
+	"io/ioutil"
+	"swifty"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/credentials"
 )
 
-func s3session() *s3.S3 {
-	addr := os.Getenv("MWARE_S3IMAGES_ADDR")
-	akey := os.Getenv("MWARE_S3IMAGES_KEY")
-	asec := os.Getenv("MWARE_S3IMAGES_SECRET")
-
-	if addr == "" || akey == "" || asec == "" {
-		panic("No bucket attached")
+func showS3Err(err error) {
+	if aerr, ok := err.(awserr.Error); ok {
+		switch aerr.Code() {
+		case s3.ErrCodeNoSuchBucket:
+			fmt.Println(s3.ErrCodeNoSuchBucket, aerr.Error())
+		default:
+			fmt.Println(aerr.Error())
+		}
+	} else {
+		fmt.Println(err.Error())
 	}
-
-	ses := session.Must(session.NewSessionWithOptions(session.Options{
-		Config: aws.Config{
-			Region: aws.String("internal"),
-			Credentials: credentials.NewStaticCredentials(akey, asec, ""),
-			Endpoint: aws.String("http://" + addr),
-			S3ForcePathStyle: aws.Bool(true),
-			S3UseAccelerate: aws.Bool(false),
-		},
-	}))
-
-	return s3.New(ses)
 }
 
 func Main(args map[string]string) interface{} {
-	svc := s3session()
-
-	input := &s3.ListObjectsV2Input{
-		Bucket:  aws.String("images"),
-	}
-
-	result, err := svc.ListObjectsV2(input)
+	svc, err := swifty.S3BucketProt("images", "http")
 	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case s3.ErrCodeNoSuchBucket:
-				fmt.Println(s3.ErrCodeNoSuchBucket, aerr.Error())
-			default:
-				fmt.Println(aerr.Error())
-			}
-		} else {
-			fmt.Println(err.Error())
+		panic("Can't get bkt")
+	}
+
+	if args["action"] == "list" {
+		input := &s3.ListObjectsV2Input{
+			Bucket:  aws.String("images"),
 		}
-		panic("Can't list objects")
+
+		result, err := svc.ListObjectsV2(input)
+		if err != nil {
+			showS3Err(err)
+			panic("Can't list objects")
+		}
+
+		onames := []string{}
+		for _, obj := range result.Contents {
+			onames = append(onames, *obj.Key)
+		}
+
+		return onames
 	}
 
-	fmt.Printf("Answered:\n")
-	onames := []string{}
-	for _, obj := range result.Contents {
-		onames = append(onames, *obj.Key)
+	if args["action"] == "put" {
+		input := &s3.PutObjectInput{
+			Bucket:	aws.String("images"),
+			Key:	aws.String(args["name"]),
+			Body:	aws.ReadSeekCloser(strings.NewReader(args["_SWY_BODY_"])),
+		}
+
+		_, err := svc.PutObject(input)
+		if err != nil {
+			showS3Err(err)
+			panic("Can't put object")
+		}
+
+		return "OK"
 	}
 
-	return onames
-}
+	if args["action"] == "get" {
+		input := &s3.GetObjectInput{
+			Bucket: aws.String("images"),
+			Key:    aws.String(args["name"]),
+		}
 
-func main() {
-	Main(nil)
+		result, err := svc.GetObject(input)
+		if err != nil {
+			showS3Err(err)
+			panic("Can't put object")
+		}
+
+		v, err := ioutil.ReadAll(result.Body)
+		if err != nil {
+			panic(fmt.Errorf("Can't read body: %s", err.Error()))
+		}
+
+		return string(v)
+	}
+
+	return "Bad request"
 }
