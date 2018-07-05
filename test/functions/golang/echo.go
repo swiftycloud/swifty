d385bb1e1e9c758b925a1564087a18cde7ee4577
Pavel Emelyanov <ovzxemul@gmail.com>
2018-07-05 11:04:46 +0000

    test: Update a bit
    Signed-off-by: Pavel Emelyanov <ovzxemul@gmail.com>

--- test/functions/golang/echo.go
+++ test/functions/golang/echo.go
@@ -5,6 +5,7 @@ import (
 )
 
 func Main(req *Request) (interface{}, *Responce) {
-	fmt.Printf("%v\n", req)
+	fmt.Printf("Rq: %v\n", req)
+	fmt.Printf("Claims: %v\n", req.Claims)
 	return map[string]string{"message": "hw:golang:" + req.Args["name"]}, &Responce{Status: 201}
 }
