2047d8111196bdaadf74c83736fd16ad60da4e23
Pavel Emelyanov <ovzxemul@gmail.com>
2018-09-11 10:24:54 +0200

    gate: Move context stuff into separate source file
    Signed-off-by: Pavel Emelyanov <ovzxemul@gmail.com>

--- src/gate/main.go
+++ src/gate/main.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/gorilla/mux"
 
-	"gopkg.in/mgo.v2"
 	"encoding/json"
 	"encoding/hex"
 	"net/http"
@@ -16,7 +15,6 @@ import (
 	"strings"
 	"context"
 	"strconv"
-	"sync/atomic"
 	"time"
 	"fmt"
 	"os"
@@ -81,46 +79,6 @@ var CORS_Clnt_Methods = []string {
 	http.MethodGet,
 	http.MethodDelete,
 	http.MethodHead,
-}
-
-type gateContext struct {
-	context.Context
-	Tenant	string
-	Admin	bool
-	ReqId	uint64
-	S	*mgo.Session
-}
-
-var reqIds uint64
-
-func mkContext2(tenant string, admin bool) (context.Context, func(context.Context)) {
-	gatectx := &gateContext{
-		context.Background(),
-		tenant,
-		admin,
-		atomic.AddUint64(&reqIds, 1),
-		session.Copy(),
-	}
-
-	return gatectx, func(ctx context.Context) { gctx(ctx).S.Close() }
-}
-
-func mkContext(tenant string) (context.Context, func(context.Context)) {
-	return mkContext2(tenant, true) /* Internal contexts are admin always! */
-}
-
-func gctx(ctx context.Context) *gateContext {
-	return ctx.(*gateContext)
-}
-
-func (gx *gateContext)tpush(tenant string) string {
-	x := gx.Tenant
-	gx.Tenant = tenant
-	return x
-}
-
-func (gx *gateContext)tpop(tenant string) {
-	gx.Tenant = tenant
 }
 
 func ctxlog(ctx context.Context) *zap.SugaredLogger {
