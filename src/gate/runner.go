5be0cad61b83cf2346f7d43b2c26af0cd138bb02
Pavel Emelyanov <ovzxemul@gmail.com>
2018-10-05 15:29:14 +0200

    gate: Keep mgo session open when saving logs
    Signed-off-by: Pavel Emelyanov <ovzxemul@gmail.com>

--- src/gate/runner.go
+++ src/gate/runner.go
@@ -130,7 +130,11 @@ func (conn *podConn)Run(ctx context.Context, sopq *statsOpaque, suff, event stri
 	}
 
 	if res.Stdout != "" || res.Stderr != "" {
-		go logSaveResult(ctx, conn.Cookie, event, res.Stdout, res.Stderr)
+		go func() {
+			sctx, done := mkContext("::logsave")
+			defer done(sctx)
+			logSaveResult(sctx, conn.Cookie, event, res.Stdout, res.Stderr)
+		}()
 	}
 
 	return res, nil
