2eea50c6cbafe0d7b7b96d3934e770aa6d4c17fb
Pavel Emelyanov <ovzxemul@gmail.com>
2018-10-15 11:44:42 +0200

    test: Chat server and client
    Signed-off-by: Pavel Emelyanov <ovzxemul@gmail.com>

---
+++ test/functions/golang/chat-cln.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/url"
+	"os"
+	"fmt"
+	"bufio"
+
+	"github.com/gorilla/websocket"
+)
+
+var addr = flag.String("addr", "localhost:8080", "http service address")
+var path = flag.String("path", "", "ws path")
+
+func main() {
+	flag.Parse()
+	log.SetFlags(0)
+
+	u := url.URL{Scheme: "ws", Host: *addr, Path: *path}
+	log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	log.Printf("connected\n")
+	fmt.Print("_: ")
+
+	go func() {
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read error:", err)
+				return
+			}
+			log.Printf(":> %s\n", message)
+			fmt.Print("_: ")
+		}
+	}()
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		text, _ := reader.ReadString('\n')
+		if text == "" {
+			break
+		}
+
+		err := c.WriteMessage(websocket.TextMessage, []byte(text))
+		if err != nil {
+			log.Println("write:", err)
+			return
+		}
+	}
+
+	c.Close()
+}
--- test/functions/golang/chat.go
+++ test/functions/golang/chat.go
@@ -6,28 +6,20 @@ import (
 	"bytes"
 	"net/http"
 	"encoding/json"
+	"encoding/base64"
 )
 
 func Main(rq *Request) (interface{}, *Responce) {
-	fmt.Printf("Go message\n")
-	fmt.Printf("`- from: %s\n", rq.Args["cid"])
-	fmt.Printf("`- type: %s\n", rq.Args["mtype"])
-	fmt.Printf("`- message: [%s]\n", rq.Body)
-
 	url := os.Getenv("MWARE_WEBSOCKETCHAT_URL")
-	url = "http://159.69.216.175:8684/websockets/3557960b7e5b5b26cee6b8d70675c7736c783b69043bfff0ec4fc13f49a02376"
-	fmt.Printf("WS: %s\n", url)
-
 	tok := os.Getenv("MWARE_WEBSOCKETCHAT_TOKEN")
-	fmt.Printf(" `: %s\n", tok)
 
+	data, err := base64.StdEncoding.DecodeString(rq.Body)
 	body, _ := json.Marshal(map[string]interface{} {
 		"msg_type": 1,
-		"msg_payload": []byte("Text message"),
+		"msg_payload": []byte(data),
 	})
 
-	fmt.Printf("-> REQ with [%s]\n", string(body))
-	req, err := http.NewRequest("POST", url + "/conns/" + rq.Args["cid"], bytes.NewBuffer(body))
+	req, err := http.NewRequest("POST", url + "/conns", bytes.NewBuffer(body))
 	if err != nil {
 		fmt.Printf("Error req: %s\n", err.Error())
 		return "ok", nil
@@ -36,7 +28,6 @@ func Main(rq *Request) (interface{}, *Responce) {
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("X-WS-Token", tok)
 
-	fmt.Printf("-> POST to [%s]\n", url + "/" + rq.Args["cid"])
 	_, err = http.DefaultClient.Do(req)
 	if err != nil {
 		fmt.Printf("Error post: %s\n", err.Error())
