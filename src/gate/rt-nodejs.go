11d9c88e21fa460ca5268981960b0b0ada5a3f9b
Pavel Emelyanov <ovzxemul@gmail.com>
2018-10-26 15:57:15 +0200

    node: Install and pass packages
    Signed-off-by: Pavel Emelyanov <ovzxemul@gmail.com>

--- src/gate/rt-nodejs.go
+++ src/gate/rt-nodejs.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"strings"
+	"context"
+	"bytes"
+	"os"
+	"os/exec"
 )
 
 var nodejs_info = langInfo {
@@ -19,5 +23,40 @@ var nodejs_info = langInfo {
 		}
 		return ret
 	},
+
+	Install:	npmInstall,
+	RunPkgPath:	nodeModules,
+}
+
+func nodeModules(id SwoId) (string, string) {
+	/*
+	 * Node's runner-js.sh sets /home/packages/node_modules as NODE_PATH
+	 */
+	return packagesDir() + "/" + id.Tennant + "/nodejs", "/home/packages"
+}
+
+func npmInstall(ctx context.Context, id SwoId) error {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+
+	tgt_dir := packagesDir() + "/" + id.Tennant + "/nodejs"
+	os.MkdirAll(tgt_dir, 0755)
+	/*
+	 * Node's container sets HOME to /home/swifty and npm installs stuff
+	 * there as it happens to be it's wdir for this launch
+	 */
+	args := []string{"run", "--rm", "-v", tgt_dir + ":/home/swifty", rtLangImage("nodejs"),
+				"npm", "install", "--no-save", id.Name}
+	ctxlog(ctx).Debugf("Running docker %v", args)
+	cmd := exec.Command("docker", args...)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		logSaveResult(ctx, id.PCookie(), "pkg_install", stdout.String(), stderr.String())
+		return err
+	}
+
+	return nil
 }
 
