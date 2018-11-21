58a1cbdfc5c183967d599fdd4e98935ee72ee844
Pavel Emelyanov <ovzxemul@gmail.com>
2018-11-21 18:48:40 +0000

    mongo: Flag to parse addresses
    Signed-off-by: Pavel Emelyanov <ovzxemul@gmail.com>

--- src/mongoproxy/main.go
+++ src/mongoproxy/main.go
@@ -1,9 +1,20 @@
 package main
 
+import (
+	"flag"
+)
+
 func main() {
 	pipelineAdd(&rqShow{})
 
-	p := makeProxy(":27018", "127.0.0.1:27017")
+	var from string
+	var to string
+
+	flag.StringVar(&from, "from", ":27018", "Where to listen for connections")
+	flag.StringVar(&to, "to", "127.0.0.1:27017", "Where to connect for mongo")
+	flag.Parse()
+
+	p := makeProxy(from, to)
 	if p == nil {
 		return
 	}
