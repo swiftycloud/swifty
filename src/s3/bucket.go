ed1e39425c1739d811b80c91c2085c36e8d1dda3
Pavel Emelyanov <ovzxemul@gmail.com>
2018-12-04 17:53:26 +0000

    s3: Introduce baucketAcct
    Signed-off-by: Pavel Emelyanov <ovzxemul@gmail.com>

--- src/s3/bucket.go
+++ src/s3/bucket.go
@@ -98,10 +98,13 @@ func RemoveFromDB(ctx context.Context, bucket *s3mgo.Bucket) (error) {
 	return dbS3RemoveOnState(ctx, bucket, S3StateInactive, query)
 }
 
+func bucketAcct(ctx context.Context, b *s3mgo.Bucket, upd bson.M) error {
+	return dbS3Update(ctx, bson.M{ "state": S3StateActive }, bson.M{ "$inc": upd }, true, b)
+}
+
 func commitObj(ctx context.Context, bucket *s3mgo.Bucket, size int64) (error) {
 	m := bson.M{ "ref": -1 }
-	err := dbS3Update(ctx, bson.M{ "state": S3StateActive },
-		bson.M{ "$inc": m }, true, bucket)
+	err := bucketAcct(ctx, bucket, m)
 	if err != nil {
 		log.Errorf("s3: Can't commit %d bytes %s: %s",
 			size, infoLong(bucket), err.Error())
@@ -110,16 +113,16 @@ func commitObj(ctx context.Context, bucket *s3mgo.Bucket, size int64) (error) {
 }
 
 func acctObj(ctx context.Context, bucket *s3mgo.Bucket, size int64) (error) {
-	err := StatsAcct(ctx, bucket.NamespaceID, bson.M{ "cnt-objects": 1, "cnt-bytes": size })
+	m := bson.M{ "cnt-objects": 1, "cnt-bytes": size }
+	err := StatsAcct(ctx, bucket.NamespaceID, m)
 	if err != nil {
 		log.Errorf("s3: Can't +account %d bytes %s: %s",
 			size, infoLong(bucket), err.Error())
 		return err
 	}
 
-	m := bson.M{ "cnt-objects": 1, "cnt-bytes": size, "ref": 1, "rover": int64(1) }
-	err = dbS3Update(ctx, bson.M{ "state": S3StateActive },
-		bson.M{ "$inc": m }, true, bucket)
+	m = bson.M{ "cnt-objects": 1, "cnt-bytes": size, "ref": 1, "rover": int64(1) }
+	err = bucketAcct(ctx, bucket, m)
 	if err != nil {
 		log.Errorf("s3: Can't +account %d bytes %s: %s",
 			size, infoLong(bucket), err.Error())
@@ -134,8 +137,7 @@ func unacctObj(ctx context.Context, bucket *s3mgo.Bucket, size int64, dropref bo
 	if dropref {
 		m["ref"] = -1
 	}
-	err := dbS3Update(ctx, bson.M{ "state": S3StateActive },
-		bson.M{ "$inc": m }, true, bucket)
+	err := bucketAcct(ctx, bucket, m)
 	if err != nil {
 		log.Errorf("s3: Can't -account %d bytes %s: %s",
 			size, infoLong(bucket), err.Error())
