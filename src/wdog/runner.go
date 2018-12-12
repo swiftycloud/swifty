f0e213ebcb5351f18255500f98e41bea9dbde201
Pavel Emelyanov <ovzxemul@gmail.com>
2018-12-12 09:54:27 +0000

    wdog: Move the code around
    Signed-off-by: Pavel Emelyanov <ovzxemul@gmail.com>

--- src/wdog/runner.go
+++ src/wdog/runner.go
@@ -7,15 +7,74 @@ package main
 
 import (
 	"sync"
-	"swifty/common/xqueue"
 	"os"
 	"os/exec"
 	"strings"
-	"net"
+	"time"
 	"fmt"
 	"syscall"
 	"strconv"
+	"net/http"
+	"io"
+
+	"swifty/apis"
+	"swifty/common/xqueue"
+	"swifty/common/http"
 )
+
+type RunnerRes struct {
+	/* Runner return code. 0 for OK, non zero for code-run error (e.g. exception) */
+	Res	int
+	/* Status the code wants to propagate back to caller */
+	Status	int
+	/* JSON-encoded return value of a function */
+	Ret	string
+}
+
+func doRun(runner *Runner, body []byte) (*swyapi.WdogFunctionRunResult, error) {
+	var err error
+
+	start := time.Now()
+	err = runner.q.SendBytes(body)
+	if err != nil {
+		log.Debugf("%s", readLines(runner.fin))
+		log.Debugf("%s", readLines(runner.fine))
+		return nil, fmt.Errorf("Can't send args: %s", err.Error())
+	}
+
+	var out RunnerRes
+	err = runner.q.Recv(&out)
+
+	ret := &swyapi.WdogFunctionRunResult{
+		Stdout: readLines(runner.fin),
+		Stderr: readLines(runner.fine),
+		Time: uint(time.Since(start) / time.Microsecond),
+	}
+
+	if err == nil {
+		if out.Res == 0 {
+			ret.Code = out.Status
+		} else {
+			ret.Code = -http.StatusInternalServerError
+		}
+		ret.Return = out.Ret
+	} else {
+		switch {
+		case err == io.EOF:
+			ret.Code = -http.StatusInternalServerError
+			ret.Return = "exited"
+		case err == xqueue.TIMEOUT:
+			ret.Code = -xhttp.StatusTimeoutOccurred
+			ret.Return = "timeout"
+		default:
+			log.Errorf("Can't read data back: %s", err.Error())
+			ret.Code = -http.StatusInternalServerError
+			ret.Return = "unknown"
+		}
+	}
+
+	return ret, nil
+}
 
 const lRunner = "/usr/bin/start_runner.sh"
 
@@ -128,9 +187,3 @@ type localRunner struct {
 	fout	string
 	ferr	string
 }
-
-type proxyRunner struct {
-	wc	*net.UnixConn
-	rkey	string
-}
-
