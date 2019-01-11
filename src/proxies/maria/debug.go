6c4f55b125f0a21768de984fbcd81c2ce4fbcc4f
Pavel Emelyanov <ovzxemul@gmail.com>
2019-01-11 17:14:34 +0000

    maria-proxy: Parse and show commands
    Signed-off-by: Pavel Emelyanov <ovzxemul@gmail.com>

--- src/proxies/maria/debug.go
+++ src/proxies/maria/debug.go
@@ -31,5 +31,20 @@ func (*rqShow)request(conid string, rq *maria_req) error {
 }
 
 func (rq *maria_req)show(conid string) {
+	if rq.cmd != 0xff {
+		if showAll {
+			switch rq.cmd {
+			case COM_SLEEP, COM_QUIT, COM_INIT_DB, COM_QUERY, COM_FIELD_LIST, COM_CREATE_DB,
+					COM_DROP_DB, COM_REFRESH, COM_SHUTDOWN, COM_STATISTICS, COM_PROCESS_INFO,
+					COM_CONNECT, COM_PROCESS_KILL, COM_DEBUG, COM_PING, COM_TIME, COM_DELAYED_INSERT,
+					COM_CHANGE_USER, COM_RESET_CONNECTION, COM_DAEMON:
+				log.Printf("%s: %d:%s\n", conid, rq.cmd, string(rq.data[1:]))
+			default:
+				log.Printf("%s: %d---\n%s---\n", conid, rq.cmd, hex.Dump(rq.data[1:]))
+			}
+		}
+		return
+	}
+
 	log.Printf("%s: %d ======\n%s============================\n", conid, rq.seq, hex.Dump(rq.data))
 }
