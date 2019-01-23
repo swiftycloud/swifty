972ffdb3d3470eee8079a7d0b490165ec4015500
Pavel Emelyanov <ovzxemul@gmail.com>
2019-01-23 17:56:09 +0300

    thens: Add more details
    Signed-off-by: Pavel Emelyanov <ovzxemul@gmail.com>

--- src/apis/thens.go
+++ src/apis/thens.go
@@ -9,7 +9,16 @@ type Then struct {
 	Call		*ThenCall		`json:"call,omitempty"`
 }
 
+/*
+ * This "then" makes gate call another function with the given
+ * args and passing the return value to its body.
+ *
+ * Then @sync field specifies whether the execution of this fn
+ * should be done synchronously and (!) the result of it is what
+ * gate would return to the caller.
+ */
 type ThenCall struct {
 	Name		string			`json:"name"`
 	Args		map[string]string	`json:"args"`
+	Sync		bool			`json:"sync"`
 }
